adbf74bf1f12a862724a3e388bb47878e8846e73
Oleksii Kuzmenko <alexey.kuzmenko1101@gmail.com>
2023-08-30 15:27:56 +0300

    Added modules controllers and dto
    This adds controllers for all API modules. Also added dto's for modules.

---
+++ src/product/product.model.ts
@@ -0,0 +1,16 @@
+export class ProductModel {
+    _id: string;
+    image: string;
+    title: string;
+    price: number;
+    oldPrice: number;
+    credit: number;
+    calcRating: number;
+    description: string;
+    disAdvantages: string;
+    categories: Array<string>;
+    tags: string;
+    characteristics: {
+        [key: string]: string;
+    };
+}
